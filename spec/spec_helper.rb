105a14910459b2660e51f48b0c4314978f7265fd
Brian Lopez <seniorlopez@gmail.com>
2009-05-18 18:04:18 -0700

    update spec_helper, adding 1.9 string method that's used in tests for 1.8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 # encoding: UTF-8
-require 'bzip2'
+require 'bzip2_ext'
+
+# back-port 1.9 method so the tests will pass in 1.8 as well
+if RUBY_VERSION.include?("1.8")
+  class String
+    def getbyte(idx)
+      self[idx]
+    end
+  end
+end
