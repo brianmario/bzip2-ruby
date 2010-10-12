35b680e9f98029a28b513a884dcd09e23d33fcb9
Alex Crichton <alex@alexcrichton.com>
2010-10-12 19:01:52 -0400

    Add documentation for an overview of the Bzip2 module

--- lib/bzip2.rb
+++ lib/bzip2.rb
@@ -1,2 +1,13 @@
 require 'bzip2_ext'
 require 'bzip2/version'
+
+# This is the base module for the +bzip2-ruby+ gem. Beneath it are the classes
+# for writing and reading data from bzip2 compressed and uncompressed streams.
+#
+# For example usage, see the Bzip2::Reader or Bzip2::Writer or the
+# {README}[link:docs/file/README.rdoc]
+#
+# @see Bzip2::Writer
+# @see Bzip2::Reader
+module Bzip2
+end
