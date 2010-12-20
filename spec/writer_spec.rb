833a2597c2735491bed00964f633921fe6253f20
Alex Crichton <alex@alexcrichton.com>
2010-12-19 22:57:50 -0600

    Use rspec's `let` instead of instance variables to pass things around

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -2,49 +2,47 @@
 require 'spec_helper'
 
 describe Bzip2::Writer do
+  let(:file){ File.expand_path('../_10lines_', __FILE__) }
+
   class Dummy
     def to_s
       "dummy"
     end
   end
 
-  before(:each) do
-    @file = File.expand_path('../_10lines_', __FILE__)
-  end
-
   after(:each) do
-    File.delete(@file) if File.exists?(@file)
+    File.delete(file) if File.exists?(file)
   end
 
   it "performs like IO#<< when using the #<< method" do
-    Bzip2::Writer.open(@file, "w") do |file|
+    Bzip2::Writer.open(file, "w") do |file|
       file << 1 << "\n" << Dummy.new << "\n" << "cat\n"
     end
     expected = [ "1\n", "dummy\n", "cat\n"]
     actual   = []
-    Bzip2::Reader.foreach(@file){ |line| actual.push line }
+    Bzip2::Reader.foreach(file){ |line| actual.push line }
     actual.should == expected
   end
 
   it "doesn't immediately flush the data when written to" do
-    io = File.new(@file, "w")
+    io = File.new(file, "w")
     bz2 = Bzip2::Writer.new(io)
     bz2 << 1 << "\n" << Dummy.new << "\n" << "cat\n"
-    bz = Bzip2::Reader.new(@file)
+    bz = Bzip2::Reader.new(file)
     lambda { bz.gets }.should raise_error(Bzip2::Error)
-    bz = Bzip2::Reader.open(@file)
+    bz = Bzip2::Reader.open(file)
     lambda { bz.gets }.should raise_error(Bzip2::Error)
     io.close
     lambda { Bzip2::Reader.new(io) }.should raise_error(IOError)
   end
 
   it "behaves the same as IO#print when using #print" do
-    Bzip2::Writer.open(@file) do |file|
+    Bzip2::Writer.open(file) do |file|
       file.print "foo\n" * 4096, "\n" * 4096,
                  "bar" * 4096, "\n" * 4096, "zot\n" * 1024
     end
 
-    Bzip2::Reader.open(@file) do |file|
+    Bzip2::Reader.open(file) do |file|
       file.gets('').should == "foo\n" * 4096 + "\n"
       file.gets('').should == "bar" * 4096 + "\n\n"
       file.gets('').should == "zot\n" * 1024
@@ -52,7 +50,7 @@ describe Bzip2::Writer do
   end
 
   it "respects specific global variables like IO#print does via #print" do
-    Bzip2::Writer.open(@file) do |file|
+    Bzip2::Writer.open(file) do |file|
       file.print "hello"
       file.print 1,2
       $_ = "wombat\n"
@@ -66,30 +64,30 @@ describe Bzip2::Writer do
       $, = nil
     end
 
-    Bzip2::Reader.open(@file) do |file|
+    Bzip2::Reader.open(file) do |file|
       file.gets(nil).should == "hello12wombat\n3,4:5,6:\n"
     end
   end
 
   it "only writes one byte via the #putc method" do
-    Bzip2::Writer.open(@file, "wb") do |file|
+    Bzip2::Writer.open(file, "wb") do |file|
       file.putc "A"
       0.upto(255) { |ch| file.putc ch }
     end
 
-    Bzip2::Reader.open(@file, "rb") do |file|
+    Bzip2::Reader.open(file, "rb") do |file|
       file.getc.should == 'A'.bytes.first
       0.upto(255) { |ch| file.getc.should == ch }
     end
   end
 
   it "behaves the same as IO#puts when using #puts" do
-    Bzip2::Writer.open(@file, "w") do |file|
+    Bzip2::Writer.open(file, "w") do |file|
       file.puts "line 1", "line 2"
       file.puts [ Dummy.new, 4 ]
     end
 
-    Bzip2::Reader.open(@file) do |file|
+    Bzip2::Reader.open(file) do |file|
       file.gets.should == "line 1\n"
       file.gets.should == "line 2\n"
       file.gets.should == "dummy\n"
@@ -98,7 +96,7 @@ describe Bzip2::Writer do
   end
 
   it "writes data successfully to a file and returns the length of the data" do
-    Bzip2::Writer.open(@file, "w") do |file|
+    Bzip2::Writer.open(file, "w") do |file|
       file.write('*' * 10).should == 10
       file.write('!' * 5).should == 5
       file.write('').should == 0
@@ -107,7 +105,7 @@ describe Bzip2::Writer do
       file.write("\n").should == 1
     end
 
-    Bzip2::Reader.open(@file) do |file|
+    Bzip2::Reader.open(file) do |file|
       file.gets.should == "**********!!!!!12.3\n"
     end
   end
